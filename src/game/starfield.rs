1db0e82ce77697b9bf8403a61fd1db9bbef07b9f
Jacques <20562845+frederickjjoubert@users.noreply.github.com>
2023-10-07 20:10:59 -0700

    recenter starfield

--- src/game/starfield.rs
+++ src/game/starfield.rs
@@ -1,8 +1,10 @@
+use crate::game::constants::{MAP_HEIGHT, MAP_WIDTH};
+use crate::game::utils::grid_to_world;
 use bevy::prelude::*;
 use bevy_hanabi::prelude::*;
 
 pub const NUMBER_OF_STARS: u32 = 1000;
-pub const RADIUS: f32 = 100.0;
+pub const RADIUS: f32 = 1000.0;
 
 pub fn spawn_star_field(mut commands: Commands, mut effects: ResMut<Assets<EffectAsset>>) {
     // Define a color gradient from white to transparent for the particles.
@@ -16,9 +18,11 @@ pub fn spawn_star_field(mut commands: Commands, mut effects: ResMut<Assets<Effec
     let lifetime = writer.lit(1_000_000_f32).expr(); // Very long lifetime
     let init_lifetime = SetAttributeModifier::new(Attribute::LIFETIME, lifetime);
 
+    let (world_x, world_y) = grid_to_world((MAP_WIDTH / 2) as i32, (MAP_HEIGHT / 2) as i32);
+
     // Create an initial position
     let init_pos = SetPositionCircleModifier {
-        center: writer.lit(Vec3::ZERO).expr(),
+        center: writer.lit(Vec3::new(world_x, world_y, 0.0)).expr(),
         axis: writer.lit(Vec3::Z).expr(),
         radius: writer.lit(RADIUS).expr(),
         dimension: ShapeDimension::Volume,
@@ -31,10 +35,10 @@ pub fn spawn_star_field(mut commands: Commands, mut effects: ResMut<Assets<Effec
         speed: writer.lit(0.1).expr(),
     };
 
-    let size = SetSizeModifier {
-        size: CpuValue::Single(Vec2::splat(0.1)),
-        screen_space_size: false,
-    };
+    // let size = SetSizeModifier {
+    //     size: CpuValue::Single(Vec2::splat(0.1)),
+    //     screen_space_size: false,
+    // };
 
     // Create a spawner
     let spawner = Spawner::once((NUMBER_OF_STARS as f32).into(), true);
@@ -46,7 +50,7 @@ pub fn spawn_star_field(mut commands: Commands, mut effects: ResMut<Assets<Effec
         .init(init_pos)
         .init(init_vel)
         .init(init_lifetime)
-        .render(size)
+        // .render(size)
         .render(ColorOverLifetimeModifier { gradient });
 
     let effect_handle = effects.add(effect);
